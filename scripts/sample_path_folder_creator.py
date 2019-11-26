61fdeede6b257c74d7f46990950d672c9c1cd7e9
Claudio Lo Giudice <51158041+claudiologiudice@users.noreply.github.com>
2019-11-26 13:49:13 +0100

    Create sample_path_folder_creator.py

---
+++ scripts/sample_path_folder_creator.py
@@ -0,0 +1,19 @@
+import os, random, string, sys
+
+try:
+        sample_status_file = sys.argv[1]
+except:
+        sys.exit('<sample_status_file>')
+ 
+def randomStringDigits(stringLenght=8):
+        return ''.join(random.choice(string.digits) for i in range(stringLenght))
+
+with open(sample_status_file, 'r') as e:
+        e.readline()
+        for line in e:
+                line = line.split(',')
+                srr_dir = line[0].strip()
+                dna_rna_folder = srr_dir + '/editing/DnaRna_' + randomStringDigits()
+                if not os.path.exists(dna_rna_folder):
+                         os.makedirs(dna_rna_folder)
+                os.system('cp ../tables/%s %s' %(srr_dir, dna_rna_folder))
